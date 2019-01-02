5bd9441ada4c3ad863506f83063073350dd310d2
Joey <joeyfenny@gmail.com>
2019-01-01 20:30:38 -0500

    node js http requests working

--- routes/leads.js
+++ routes/leads.js
@@ -81,6 +81,13 @@ router.post("/new", isSafe, function(req, res) {
 });
 
 router.post("/final-step", function(req, res) {
+
+  httpRequest('http://www.google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+
   res.render("leads/final-step", {data: req.body});
 });
 
